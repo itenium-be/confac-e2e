13cbd8681c617c2d030e19652f416ba24aa81d2d
remco Verbruggen <remco.verbruggen@itenium.be>
2025-10-23 17:18:57 +0200

    added auth logic

---
+++ tests/auth/auth.setup.ts
@@ -0,0 +1,20 @@
+import { test as setup, expect } from '@playwright/test';
+import path from 'path';
+import { LoginPage } from '../../src/pages/loginPage';
+
+const authFile = path.join(__dirname, '../../playwright/.auth/user.json');
+let login: LoginPage;
+
+
+setup('authenticate', async ({ page }) => {
+    login = new LoginPage(page);
+   
+    await login.goto();
+    await login.enterName('e2e-test-user');
+    await login.submitForm();
+
+    await expect(page).toHaveTitle("Maandelijkse facturatie - confac")
+   
+    await page.context().storageState({ path: authFile });
+
+})
